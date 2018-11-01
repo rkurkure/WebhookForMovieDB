666a0f05f1eb512b5d7cd33595249c568bae245a
rkurkure <34217585+rkurkure@users.noreply.github.com>
2018-11-01 15:46:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ server.use(bodyParser.urlencoded({
 
 server.use(bodyParser.json());
 
-server.post('/get-movie-details', (req, res) => {
+server.post('/get-movie-details', (req, fulfillment) => {
 
     const movieToSearch = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie ? req.body.queryResult.parameters.movie : 'The Godfather';
     const reqUrl = encodeURI(`http://www.omdbapi.com/?t=${movieToSearch}&apikey=${API_KEY}`);
@@ -26,14 +26,14 @@ server.post('/get-movie-details', (req, res) => {
             let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
             dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
 
-            return res.json({
+            return fulfillment.json({
                 speech: dataToSend,
                 displayText: dataToSend,
                 source: 'get-movie-details'
             });
         });
     }, (error) => {
-        return res.json({
+        return fulfillment.json({
             speech: 'Something went wrong!',
             displayText: 'Something went wrong!',
             source: 'get-movie-details'
